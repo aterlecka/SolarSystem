cb0732b06668fb1287464453ad823ebafe534f95
aterlecka <aleksandra.terlecka1996@gmail.com>
2020-06-16 15:37:39 +0200

    Create all planets and create folders for styles

---
+++ active.js
@@ -0,0 +1,13 @@
+let scene,
+    camera,
+    light,
+    renderer,
+    earthObject;
+
+let WIDTH = window.innerWidth - 30,
+    HEIGHT = window.innerHeight - 30;
+
+let angle = 45,
+    aspect = WIDTH / HEIGHT,
+    near = 0.1,
+    far = 3000;
